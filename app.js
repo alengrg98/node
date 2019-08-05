fd7edb488d396bafe740091b613d412954bf25f5
alengrg98 <alengeorgeshibu@cs.ajce.in>
2019-08-05 12:01:48 +0530

    basics

---
+++ app.js
@@ -0,0 +1,12 @@
+/*var x=function(){
+console.log("Hello World!!");
+}
+//sayhello();
+
+x();
+*/
+
+var g=require("./greeting");
+g.sayhello();
+g.f1();
+console.log(g.languages);
---
+++ greeting.js
@@ -0,0 +1,12 @@
+exports.sayhello=function()
+    {
+        console.log("Hello World!!");
+    }
+
+    exports.f1=function()
+    {
+        console.log("F!!");
+    }
+
+    exports.languages=["C","C++","JAVA"];
+    
